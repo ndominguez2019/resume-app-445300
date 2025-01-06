5dd68779308285d478907caaf931733008cc15c8
ndominguez2019 <ndominguez2019@gmail.com>
2025-01-06 02:45:12 +0000

    Initial commit

---
+++ blogproject/wsgi.py
@@ -0,0 +1,19 @@
+import sys
+import os
+
+# Add the path to the project and virtual environment to the system path
+sys.path.append('/home/ndominguez2019/blogproject')
+sys.path.append('/home/ndominguez2019/blogproject/blogproject')
+
+# Set the DJANGO_SETTINGS_MODULE environment variable to your project's settings
+os.environ['DJANGO_SETTINGS_MODULE'] = 'blogproject.settings'
+
+# Directly add site-packages to sys.path for the virtual environment
+site_packages = '/home/ndominguez2019/blogproject/blog_env/lib/python3.8/site-packages'
+sys.path.append(site_packages)
+
+# Import Django's WSGI application handler
+from django.core.wsgi import get_wsgi_application
+
+# Get the application for mod_wsgi to run
+application = get_wsgi_application()
